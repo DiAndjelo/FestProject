a42481a4cc8deba33721a131a54edfa379f35af8
satio <satioidou2se@gmail.com>
2020-06-03 02:14:23 +0500

    save img files

--- FestProject/settings.py
+++ FestProject/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'FestProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'FestProjectDB',
-        'USER': 'admin',
-        'PASSWORD': 'WolfShopPass746528375',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
