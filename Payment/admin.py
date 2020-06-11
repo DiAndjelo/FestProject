267d16519e426efd256e9f72bfa7591f031bc5bc
DiAndjelo <51744315+diandjelo@users.noreply.github.com>
2020-06-12 04:05:40 +0500

    adding Payment models for Admin panel and Payment integration into the front

--- Payment/admin.py
+++ Payment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from Payment.models import Ticket, TicketPay, PaymentModel
+
+admin.site.register(Ticket)
+admin.site.register(TicketPay)
+admin.site.register(PaymentModel)
--- Payment/models.py
+++ Payment/models.py
@@ -1,7 +1,80 @@
 from django.db import models
+from random import randint
 
-# Create your models here.
+
+class Ticket(models.Model):
+    """ Модель Билета
+    """
+    name = models.CharField('Название', max_length=64)
+    price = models.DecimalField("Стоимость", default=0, max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return "%s %s RUB " % (self.name, int(self.price))
+
+    class Meta:
+        verbose_name = "Билет"
+        verbose_name_plural = "Билеты"
 
 
-# class PaymentModel(models.Model):
-#     value =
+class TicketPay(models.Model):
+    """ Модель принадлежности билета + генерация индивидуального кода
+    """
+    customer_name = models.CharField("Имя", max_length=64, blank=False, null=False, default=None)
+    customer_surname = models.CharField("Фамилия", max_length=64, blank=False, null=False, default=None)
+    customer_email = models.EmailField("Email", max_length=128, blank=False, null=False, default=None)
+    customer_phone = models.CharField("Телефон", max_length=48, blank=True, null=True, default=None)
+    generated_code = models.CharField("Персональный ключ", max_length=6, blank=True, null=False)
+    created = models.DateTimeField("Создано", auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField("Обновлено", auto_now_add=False, auto_now=True)
+
+    def save(self, *args, **kwargs):
+        b = ''
+        alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                    'U', 'V', 'W', 'X', 'Y', 'Z',
+                    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u', 'v', 'w', 'x', 'y', 'z',
+                    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+        for i in range(5):
+            a = randint(0, 35)
+            b = str(b) + str(alphabet[a])
+
+        self.generated_code = b
+
+        super(TicketPay, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "%s %s" % (self.customer_surname, self.customer_name)
+
+    class Meta:
+        verbose_name = "Заказанный билет"
+        verbose_name_plural = "Заказанные билеты"
+
+
+class PaymentModel(models.Model):
+    """ Модель оплаты заказа. Здесь считается количество заказанных билетов и их стоимость. Отсюда будем брать сумму
+        и передавать в ЯКассы
+    """
+    order = models.ManyToManyField(TicketPay, verbose_name="Заказ", blank=True, default=None)
+    ticket = models.ForeignKey(Ticket, verbose_name="Билет", blank=True, default=1, null=True,
+                               on_delete=models.SET_NULL)
+    nmb = models.IntegerField("Количество", default=1)
+    price_per_item = models.DecimalField("Стоимость", default=0, max_digits=10, decimal_places=2)
+    total_price = models.DecimalField("Сумма", default=0, max_digits=10, decimal_places=2)  # price*nmb
+    created = models.DateTimeField("Создано", auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField("Обновлено", auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s Билета(ов). В сумме %s RUB" % (self.nmb, int(self.total_price))
+
+    class Meta:
+        verbose_name = "Билет в заказе"
+        verbose_name_plural = "Билеты в заказе"
+
+    def save(self, *args, **kwargs):
+        price_per_item = self.ticket.price
+        self.price_per_item = price_per_item
+        self.nmb = self.order.count()
+        self.total_price = int(self.nmb) * price_per_item
+
+        super(PaymentModel, self).save(*args, **kwargs)
